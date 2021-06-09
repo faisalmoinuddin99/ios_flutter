b928cb9638f0f5d70be35c563725cf773c105eb9
faisal <faisal25march99@gmail.com>
2021-06-09 21:24:19 +0530

    Done with Spanish Number Project

--- my_statefull/lib/main.dart
+++ my_statefull/lib/main.dart
@@ -1,14 +1,18 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyButton(),
   ));
 }
 
-class MyButton extends StatefulWidget {}
+class MyButton extends StatefulWidget {
+  @override
+  MyButtonState createState() {
+    return MyButtonState();
+  }
+}
 
 class MyButtonState extends State<MyButton> {
   int counter = 0;
@@ -24,7 +28,31 @@ class MyButtonState extends State<MyButton> {
     "nueve",
     "dietz"
   ];
+  List<String> nativeNumber = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
   String defaultText = "Spanish Numbers";
+
+  void displaySNumbers() {
+    setState(() {
+      defaultText = spanishNumber[counter] + " " + nativeNumber[counter];
+
+      if (counter < 9) {
+        counter += 1;
+      } else {
+        counter = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext) {
@@ -45,13 +73,22 @@ class MyButtonState extends State<MyButton> {
                 Padding(
                   padding: EdgeInsets.all(10.0),
                 ),
-                RaisedButton(
-                  child: Text('Call number'),
-                  onPressed: () {},
-                  color: Colors.orange,
-                )
+                // RaisedButton(
+                //   child: Text(
+                //     'Call number',
+                //     style: TextStyle(color: Colors.white),
+                //   ),
+                //   onPressed: displaySNumbers,
+                //   color: Colors.orange,
+                // )
               ]),
         ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: displaySNumbers,
+        child: Icon(Icons.add_box),
       ),
     );
   }
